2fe12ee5be4167d8053007f1183fd9ddb8b31840
Camila Arredondo <cami.arredondo95@gmail.com>
2023-12-14 20:40:34 -0300

    agregar vistas generales

---
+++ src/Pages/Admision/fichaDocente.tsx
@@ -0,0 +1,200 @@
+import { useState } from 'react'
+import { Disclosure, RadioGroup, Tab } from '@headlessui/react'
+import { StarIcon } from '@heroicons/react/20/solid'
+import { HeartIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/outline'
+
+const datos = {
+    name: 'Antonio Toro',
+    ciclo: 'Media',
+  images: [
+    {
+      id: 1,
+      name: 'Angled view',
+      src: 'https://tailwindui.com/img/ecommerce-images/product-page-03-product-01.jpg',
+      alt: 'Angled front view with bag zipped and handles upright.',
+    },
+    // More images...
+  ],
+
+  description: `
+    <p>Rut Nacionalidad Fecha de nacimieto Edad 
+    N° de telefono, Direccion</p>
+  `,
+  details: [
+    {
+        name: 'Estudios',
+        items: [
+          'Multiple strap configurations',
+          'Spacious interior with top zip',
+          'Leather handle and tabs',
+          'Interior dividers',
+          'Stainless strap loops',
+          'Double stitched construction',
+          'Water-resistant',
+        ],
+      },
+      {
+        name: 'Asignaturas',
+        items: [
+          'Multiple strap configurations',
+          'Spacious interior with top zip',
+          'Leather handle and tabs',
+          'Interior dividers',
+          'Stainless strap loops',
+          'Double stitched construction',
+          'Water-resistant',
+        ],
+      },
+      {
+        name: 'Cursos',
+        items: [
+          'Multiple strap configurations',
+          'Spacious interior with top zip',
+          'Leather handle and tabs',
+          'Interior dividers',
+          'Stainless strap loops',
+          'Double stitched construction',
+          'Water-resistant',
+        ],
+      },
+      {
+        name: 'Contacto de emergencia',
+        items: [
+          'Multiple strap configurations',
+          'Spacious interior with top zip',
+          'Leather handle and tabs',
+          'Interior dividers',
+          'Stainless strap loops',
+          'Double stitched construction',
+          'Water-resistant',
+        ],
+      },
+    
+    // More sections...
+  ],
+}
+function classNames(...classes:any[]) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+export default function FichaDocente() {
+  
+    return (
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/* Image gallery */}
+            <Tab.Group as="div" className="flex flex-col-reverse">
+              {/* Image selector */}
+              <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+                <Tab.List className="grid grid-cols-4 gap-6">
+                  {datos.images.map((image) => (
+                    <Tab
+                      key={image.id}
+                      className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                    >
+                      {({ selected }) => (
+                        <>
+                          <span className="sr-only">{image.name}</span>
+                          <span className="absolute inset-0 overflow-hidden rounded-md">
+                            <img src={image.src} alt="" className="h-full w-full object-cover object-center" />
+                          </span>
+                          <span
+                            className={classNames(
+                              selected ? 'ring-indigo-500' : 'ring-transparent',
+                              'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2'
+                            )}
+                            aria-hidden="true"
+                          />
+                        </>
+                      )}
+                    </Tab>
+                  ))}
+                </Tab.List>
+              </div>
+  
+              <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
+                {datos.images.map((image) => (
+                  <Tab.Panel key={image.id}>
+                    <img
+                      src={image.src}
+                      alt={image.alt}
+                      className="h-full w-full object-cover object-center sm:rounded-lg"
+                    />
+                  </Tab.Panel>
+                ))}
+              </Tab.Panels>
+            </Tab.Group>
+  
+            {/* Product info */}
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">{datos.name}</h1>
+              <h2 className="text-3xl text-base tracking-tight text-gray-900">{datos.ciclo}</h2>
+
+  
+              {/* Reviews */}
+              <div className="mt-3">
+
+              </div>
+  
+              <div className="mt-6">
+                <h3 className="sr-only">Description</h3>
+  
+                <div
+                  className="space-y-6 text-base text-gray-700"
+                  dangerouslySetInnerHTML={{ __html: datos.description }}
+                />
+              </div>
+
+  
+              <section aria-labelledby="details-heading" className="mt-12">
+                <h2 id="details-heading" className="sr-only">
+                  Additional details
+                </h2>
+  
+                <div className="divide-y divide-gray-200 border-t">
+                  {datos.details.map((detail) => (
+                    <Disclosure as="div" key={detail.name}>
+                      {({ open }) => (
+                        <>
+                          <h3>
+                            <Disclosure.Button className="group relative flex w-full items-center justify-between py-6 text-left">
+                              <span
+                                className={classNames(open ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-medium')}
+                              >
+                                {detail.name}
+                              </span>
+                              <span className="ml-6 flex items-center">
+                                {open ? (
+                                  <MinusIcon
+                                    className="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
+                                    aria-hidden="true"
+                                  />
+                                ) : (
+                                  <PlusIcon
+                                    className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                                    aria-hidden="true"
+                                  />
+                                )}
+                              </span>
+                            </Disclosure.Button>
+                          </h3>
+                          <Disclosure.Panel as="div" className="prose prose-sm pb-6">
+                            <ul role="list">
+                              {detail.items.map((item) => (
+                                <li key={item}>{item}</li>
+                              ))}
+                            </ul>
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  ))}
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
--- src/Rutas.tsx
+++ src/Rutas.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Admision } from "./Pages/Admision/admision";
-import { Alumno } from "./Pages/Alumnos/alumno";
+import { Alumno } from "./Pages/Alumnos/alumno.page";
 import { Docente } from "./Pages/Docente/docente";
 import { Inspectoria } from "./Pages/Inspectoria/inspectoria";
 import { Login } from "./Pages/Login/login";
@@ -13,6 +13,18 @@ import { LoginAdmision } from "./Pages/Login/loginAdmision";
 import { LoginRecaudacion } from "./Pages/Login/loginRecaudacion";
 import { LoginInspectoria } from "./Pages/Login/loginInspectoria";
 import { LoginSostenedor } from "./Pages/Login/loginSostenedor";
+import { FormularioNuevoAlumno } from "./Pages/Admision/nuevoAlumnoFormulario";
+import { MainComponents } from "./Components/main.components";
+import { FormularioNuevoDocente } from "./Pages/Admision/nuevoDocenteFormulario";
+import { Calendario } from "./Components/calendario.components";
+import FichaDocente from "./Pages/Admision/fichaDocente";
+import FichaAlumno from "./Pages/Admision/fichaAlumno";
+import { AllDocentesAdmision } from "./Pages/Admision/verDocentesAdmision";
+import { AllAlumnosAdmision } from "./Pages/Admision/verAlumnosAdmision";
+import { AllDocentesSostenedor } from "./Pages/Sostenedor/verDocentesSostenedor";
+import { AllAlumnosSostenedor } from "./Pages/Sostenedor/verAlumnosSostenedor";
+import { Cursos } from "./Pages/Sostenedor/cursos";
+import { Asignaturas } from "./Pages/Sostenedor/asignaturas";
 
 export function Rutas() {
     return (
@@ -21,6 +33,7 @@ export function Rutas() {
           <Route path="/" element={<Login />}></Route>
 
           <Route path="/login" element={<Login />}></Route>
+          
           <Route path="/logindocente" element={<LoginDocente />}></Route>          
           <Route path="/loginselectadmin" element={<LoginSelectAdmin />}></Route>
           <Route path="/loginalumno" element={<LoginAlumno />}></Route>
@@ -31,11 +44,38 @@ export function Rutas() {
 
 
           <Route path="/alumno" element={<Alumno />}></Route>
+
           <Route path="/docente" element={<Docente />}></Route>
+
           <Route path="/admision" element={<Admision/>}></Route>
+          <Route path="/admision/alumno/formulario/ingreso" element={<FormularioNuevoAlumno />}></Route>
+          <Route path="/admision/docente/formulario/ingreso" element={<FormularioNuevoDocente />}></Route>
+          <Route path="/admision/calendario" element={<Calendario />}></Route>
+          <Route path="/admision/docentes" element={<AllDocentesAdmision />}></Route>
+          <Route path="/admision/ficha/docente" element={<FichaDocente />}></Route>
+          <Route path="/admision/alumnos" element={<AllAlumnosAdmision />}></Route>
+          <Route path="/admision/ficha/alumno" element={<FichaAlumno />}></Route>
+
+
           <Route path="/inspectoria" element={<Inspectoria />}></Route>
+
           <Route path="/sostenedor" element={<Sostenedor />}></Route>
+          <Route path="/sostenedor/docentes" element={<AllDocentesSostenedor />}></Route>
+          <Route path="/sostenedor/ficha/docente" element={<FichaDocente />}></Route>
+          <Route path="/sostenedor/alumnos" element={<AllAlumnosSostenedor />}></Route>
+          <Route path="/sostenedor/ficha/alumno" element={<FichaAlumno />}></Route>
+          <Route path="/sostenedor/calendario" element={<Calendario />}></Route>
+          <Route path="/sostenedor/cursos" element={<Cursos />}></Route>
+          <Route path="/sostenedor/asignaturas" element={<Asignaturas />}></Route>
+
           <Route path="/recaudacion" element={<Recaudacion />}></Route>
+
+
+          <Route path="/recaudacion" element={<Calendario />}></Route>
+          
+
+
+
 
           
 
